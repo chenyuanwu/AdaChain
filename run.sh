fc5b6edb03437e865df90e91a04de7913c9e6443
chenyuanwu <1404059145@qq.com>
2022-09-04 14:47:39 -0600

    Now block size, early execution and reordering are configurable

--- run.sh
+++ run.sh
@@ -5,22 +5,23 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-cd ~/chen2/config
-rm -rf client_config.json
+cd ~/LBC/config
 #create a json file for inputs given by the user
 echo "{" >> client_config.json
-echo "\"write_ratio\": $1," >> client_config.json 
-echo "\"hot_key_ratio\": $2," >> client_config.json 
-echo "\"num_keys\": $3,"    >> client_config.json 
-echo "\"num_hot_keys\": $4,">> client_config.json 
-echo "\"trans_per_interval\": $5," >> client_config.json 
-echo "\"interval\": $6," >> client_config.json
-echo "\"execution_delay\": $7" >> client_config.json 
+echo "\"write_ratio\": $1," > client_config.json 
+echo "\"hot_key_ratio\": $2," > client_config.json 
+echo "\"num_keys\": $3,"    > client_config.json 
+echo "\"num_hot_keys\": $4,"> client_config.json 
+echo "\"trans_per_interval\": $5," > client_config.json 
+echo "\"interval\": $6," > client_config.json
+echo "\"execution_delay\": $7" > client_config.json 
 echo "}" >> client_config.json
+echo "{\"arch\": {\"blocksize\": $8, \"early_execution\": $9, \"reorder\": ${10}},\"sysconfig\": {\"num_execution_threads\": 16,\"leader\": \"10.10.1.2:50052\",\"followers\": [\"10.10.1.3:50052\",\"10.10.1.4:50052\"]}}" > peer_config.json
+
 cd ..
  
 rm -rf peer.cc
-if [[ "$8" != "" ]]; then
+if [[ "${11}" != "" ]]; then
 ln -s blocking.cc peer.cc
 else
 ln -s nonblocking.cc peer.cc
@@ -29,7 +30,7 @@ fi
 make clean
 make
 
-./peer -l -a 10.10.1.2:50052
+#./peer -l -a 10.10.1.2:50052
 #./peer -a 10.10.1.4:50052
-#./peer -a 10.10.1.4:50053
+./peer -a 10.10.1.4:50053
 #./client
