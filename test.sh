eace322909ee5b1175578dded11d15b05d2d8cf8
chenyuanwu <1404059145@qq.com>
2022-09-30 13:03:14 -0600

    fixed some script issue

--- test.sh
+++ test.sh
@@ -21,7 +21,7 @@ do
                         #timeout -s SIGINT 120 ./run.sh $write_ratio $hot_key_ratio $num_keys $num_hot_keys $trans_per_interval $interval $execution_delay $block_size $early_execution $reorder $early_abort 1 -b & sleep 120
                         #echo "write_ratio:$1 | hot_key_ratio:$2 | num_keys:$3 | num_hot_keys:$4	trans_per_interval:$5	interval:$6	execution_delay:$7	blocksize:$8	xov:${9}	reorder:${10}	early_abort:${11}	block_pipe_num:${12}";
                         #echo "write_ratio:$1 | hot_key_ratio:$2 | num_keys:$3 | num_hot_keys:$4	trans_per_interval:$5	interval:$6	execution_delay:$7	blocksize:$8	xov:${9}	reorder:${10}	early_abort:${11}	block_pipe_num:${12}";
-                        echo "early_abort: ${11} | trans_per_interval: $5 | hot_key_ratio: $2 | write_ratio: $1 | block_size: $8"
+                        echo "early_abort: $early_abort | trans_per_interval: $trans_per_interval | hot_key_ratio: $hot_key_ratio | write_ratio: $write_ratio | block_size: $block_size"
                 done
                 echo "---------------------------------------------------------------------------------------------------------"
             done
