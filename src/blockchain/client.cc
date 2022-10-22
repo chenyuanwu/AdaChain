4b881cf293b6daeb83fdbf1bfbf57ce337b729a9
chenyuanwu <1404059145@qq.com>
2022-10-21 18:49:50 -0600

    Client: switching between multiple workloads

--- src/blockchain/client.cc
+++ src/blockchain/client.cc
@@ -11,9 +11,12 @@ void *client_thread(void *arg) {
     struct CliThreadContext ctx = *(struct CliThreadContext *)arg;
     shared_ptr<grpc::Channel> peer_channel = grpc::CreateChannel(ctx.peer_grpc_endpoint, grpc::InsecureChannelCredentials());
     unique_ptr<PeerComm::Stub> stub(PeerComm::NewStub(peer_channel));
+    int num_keys = client_config["workloads"][0]["num_keys"].GetInt();
+    int num_peers = peer_config["sysconfig"]["followers"].Size() + 1;
+    int64_t duration_ms = client_config["duration"].GetInt() * 60 * 1000;
 
     /* prepopulate */
-    for (int i = 0; i < ctx.num_keys; i++) {
+    for (int i = 0; i < num_keys; i++) {
         {
             ClientContext context;
             PrepopulateResponse response;
@@ -43,16 +46,6 @@ void *client_thread(void *arg) {
     }
     LOG(INFO) << "finished prepopulating node " << ctx.peer_grpc_endpoint << ".";
 
-    /* start benchmarking */
-    random_device rd;
-    mt19937 gen(rd());
-    bernoulli_distribution is_hot(ctx.hot_key_ratio);
-    bernoulli_distribution is_update(ctx.write_ratio);
-    uniform_int_distribution<int> trans(0, 4);
-    int hot_keys_range = ctx.num_hot_keys;
-    uniform_int_distribution<int> hot_key(0, hot_keys_range - 1);
-    uniform_int_distribution<int> cold_key(hot_keys_range, ctx.num_keys - 1);
-
     pthread_barrier_wait(&prep_barrier);  // set a barrier here
 
     {
@@ -67,92 +60,117 @@ void *client_thread(void *arg) {
         }
     }
 
-    while (!end_flag) {
-        usleep(ctx.interval);
+    /* start benchmarking */
+    random_device rd;
+    mt19937 gen(rd());
+    chrono::milliseconds start = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
+    for (int64_t i = 0; i < client_config["workloads"].Size(); i++) {
+        const Value& workload = client_config["workloads"][i];
+        bernoulli_distribution is_hot(workload["hot_key_ratio"].GetDouble());
+        bernoulli_distribution is_update(workload["write_ratio"].GetDouble());
+        uniform_int_distribution<int> trans(0, 4);
+        int hot_keys_range = workload["num_hot_keys"].GetInt();
+        uniform_int_distribution<int> hot_key(0, hot_keys_range - 1);
+        uniform_int_distribution<int> cold_key(hot_keys_range, num_keys - 1);
+        
+        int execution_delay = workload["execution_delay"].GetInt();
+        int interval = workload["interval"].GetInt();
+        int trans_per_interval = workload["trans_per_interval"].GetInt() / num_peers;
 
-        for (int i = 0; (!end_flag) && (i < ctx.trans_per_interval); i++) {
-            Request req;
-            TransactionProposal *proposal = req.mutable_proposal();
-            int trans_choice = -1;
-            if (is_update(gen)) {
-                trans_choice = trans(gen);
-                if (trans_choice == 0) {
-                    proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_TransactSavings);
-                } else if (trans_choice == 1) {
-                    proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_DepositChecking);
-                } else if (trans_choice == 2) {
-                    proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_SendPayment);
-                } else if (trans_choice == 3) {
-                    proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_WriteCheck);
-                } else if (trans_choice == 4) {
-                    proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_Amalgamate);
-                }
-            } else {
-                proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_Query);
+        LOG(INFO) << "running workload " << i + 1 << ".";
+
+        while (true) {
+            chrono::milliseconds curr = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
+            int64_t time_elapsed = (curr - start).count();
+            if (time_elapsed >= (i + 1) * duration_ms) {
+                break;
             }
+            usleep(interval);
 
-            if (is_hot(gen)) {
-                string user1 = to_string(hot_key(gen));
-                if (trans_choice == 0) {
-                    proposal->add_keys("saving_" + user1);
-                } else if (trans_choice == 1) {
-                    proposal->add_keys("checking_" + user1);
-                } else if (trans_choice == 2) {
-                    proposal->add_keys("checking_" + user1);
-                    string user2 = to_string(hot_key(gen));
-                    proposal->add_keys("checking_" + user2);
-                } else if (trans_choice == 3) {
-                    proposal->add_keys("checking_" + user1);
-                } else if (trans_choice == 4) {
-                    proposal->add_keys("checking_" + user1);
-                    proposal->add_keys("saving_" + user1);
-                } else if (trans_choice == -1) {
-                    proposal->add_keys("checking_" + user1);
-                    proposal->add_keys("saving_" + user1);
+            for (int i = 0; (!end_flag) && (i < trans_per_interval); i++) {
+                Request req;
+                TransactionProposal *proposal = req.mutable_proposal();
+                int trans_choice = -1;
+                if (is_update(gen)) {
+                    trans_choice = trans(gen);
+                    if (trans_choice == 0) {
+                        proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_TransactSavings);
+                    } else if (trans_choice == 1) {
+                        proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_DepositChecking);
+                    } else if (trans_choice == 2) {
+                        proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_SendPayment);
+                    } else if (trans_choice == 3) {
+                        proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_WriteCheck);
+                    } else if (trans_choice == 4) {
+                        proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_Amalgamate);
+                    }
+                } else {
+                    proposal->set_type(TransactionProposal::Type::TransactionProposal_Type_Query);
                 }
-            } else {
-                string user1 = to_string(cold_key(gen));
-                if (trans_choice == 0) {
-                    proposal->add_keys("saving_" + user1);
-                } else if (trans_choice == 1) {
-                    proposal->add_keys("checking_" + user1);
-                } else if (trans_choice == 2) {
-                    proposal->add_keys("checking_" + user1);
-                    string user2 = to_string(cold_key(gen));
-                    proposal->add_keys("checking_" + user2);
-                } else if (trans_choice == 3) {
-                    proposal->add_keys("checking_" + user1);
-                } else if (trans_choice == 4) {
-                    proposal->add_keys("checking_" + user1);
-                    proposal->add_keys("saving_" + user1);
-                } else if (trans_choice == -1) {
-                    proposal->add_keys("checking_" + user1);
-                    proposal->add_keys("saving_" + user1);
-                }
-            }
-            proposal->set_execution_delay(ctx.execution_delay);
 
-            ClientContext context;
-            google::protobuf::Empty rsp;
-            Status status = stub->send_to_peer(&context, req, &rsp);
-            if (!status.ok()) {
-                LOG(ERROR) << "grpc failed in send_to_peer.";
-                exit(1);
+                if (is_hot(gen)) {
+                    string user1 = to_string(hot_key(gen));
+                    if (trans_choice == 0) {
+                        proposal->add_keys("saving_" + user1);
+                    } else if (trans_choice == 1) {
+                        proposal->add_keys("checking_" + user1);
+                    } else if (trans_choice == 2) {
+                        proposal->add_keys("checking_" + user1);
+                        string user2 = to_string(hot_key(gen));
+                        proposal->add_keys("checking_" + user2);
+                    } else if (trans_choice == 3) {
+                        proposal->add_keys("checking_" + user1);
+                    } else if (trans_choice == 4) {
+                        proposal->add_keys("checking_" + user1);
+                        proposal->add_keys("saving_" + user1);
+                    } else if (trans_choice == -1) {
+                        proposal->add_keys("checking_" + user1);
+                        proposal->add_keys("saving_" + user1);
+                    }
+                } else {
+                    string user1 = to_string(cold_key(gen));
+                    if (trans_choice == 0) {
+                        proposal->add_keys("saving_" + user1);
+                    } else if (trans_choice == 1) {
+                        proposal->add_keys("checking_" + user1);
+                    } else if (trans_choice == 2) {
+                        proposal->add_keys("checking_" + user1);
+                        string user2 = to_string(cold_key(gen));
+                        proposal->add_keys("checking_" + user2);
+                    } else if (trans_choice == 3) {
+                        proposal->add_keys("checking_" + user1);
+                    } else if (trans_choice == 4) {
+                        proposal->add_keys("checking_" + user1);
+                        proposal->add_keys("saving_" + user1);
+                    } else if (trans_choice == -1) {
+                        proposal->add_keys("checking_" + user1);
+                        proposal->add_keys("saving_" + user1);
+                    }
+                }
+                proposal->set_execution_delay(execution_delay);
+
+                ClientContext context;
+                google::protobuf::Empty rsp;
+                Status status = stub->send_to_peer(&context, req, &rsp);
+                if (!status.ok()) {
+                    LOG(ERROR) << "grpc failed in send_to_peer.";
+                    exit(1);
+                }
             }
         }
     }
 
-    // {
-    //     ClientContext context;
-    //     google::protobuf::Empty req;
-    //     google::protobuf::Empty rsp;
-    //     Status status = stub->end_benchmarking(&context, req, &rsp);
-    //     if (!status.ok()) {
-    //         LOG(ERROR) << "grpc failed in end_benchmarking.";
-    //     } else {
-    //         LOG(INFO) << "client thread stops benchmarking.";
-    //     }
-    // }
+    {
+        ClientContext context;
+        google::protobuf::Empty req;
+        google::protobuf::Empty rsp;
+        Status status = stub->end_benchmarking(&context, req, &rsp);
+        if (!status.ok()) {
+            LOG(ERROR) << "grpc failed in end_benchmarking.";
+        } else {
+            LOG(INFO) << "client thread stops benchmarking.";
+        }
+    }
     return nullptr;
 }
 
@@ -185,13 +203,6 @@ int main(int argc, char *argv[]) {
     struct CliThreadContext *ctxs = (struct CliThreadContext *)calloc(num_peers, sizeof(struct CliThreadContext));
     for (int i = 0; i < num_peers; i++) {
         ctxs[i].peer_grpc_endpoint = peers[i];
-        ctxs[i].num_keys = client_config["num_keys"].GetInt();
-        ctxs[i].num_hot_keys = client_config["num_hot_keys"].GetInt();
-        ctxs[i].execution_delay = client_config["execution_delay"].GetInt();
-        ctxs[i].trans_per_interval = client_config["trans_per_interval"].GetInt() / num_peers;
-        ctxs[i].interval = client_config["interval"].GetInt();
-        ctxs[i].write_ratio = client_config["write_ratio"].GetDouble();
-        ctxs[i].hot_key_ratio = client_config["hot_key_ratio"].GetDouble();
         pthread_create(&client_tids[i], NULL, client_thread, &ctxs[i]);
         /* stick thread to a core for better performance */
         int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
