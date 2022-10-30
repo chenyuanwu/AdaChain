0214ace11195273035bf9e9318dca0bda8c28aa7
Bhavana Mehta <29337200+bhavanamehta5@users.noreply.github.com>
2022-10-30 05:33:49 -0400

    debug

--- src/blockchain/smart_contracts.cc
+++ src/blockchain/smart_contracts.cc
@@ -98,6 +98,8 @@ If all the keys are a subset of the old RW set, the result is valid and can be c
 
 bool patch_up_code(Endorsement *endorsement, const string &key, struct RecordVersion record_version, struct TransactionProposal *proposal) {
     uint64_t block_id = 0;
+    uint64_t last_block_id = 0;
+
     if (proposal->type() == TransactionProposal::Type::TransactionProposal_Type_Get) {
         kv_get(key, endorsement, nullptr, block_id); 
         if((last_block_id!=0) && (block_id > last_block_id)){
